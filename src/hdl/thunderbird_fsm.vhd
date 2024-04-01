fd762ecba41f8d8f61c38a807d8929eac6b073ab
Ashleyalexis00 <156027376+Ashleyalexis00@users.noreply.github.com>
2024-04-01 17:26:11 -0600

    Add files via upload

--- src/hdl/thunderbird_fsm.vhd
+++ src/hdl/thunderbird_fsm.vhd
@@ -36,18 +36,18 @@
 --|					can be changed by the inputs
 --|					
 --|
---|                 xxx State Encoding key
+--|                 One-Hot State Encoding key
 --|                 --------------------
 --|                  State | Encoding
 --|                 --------------------
---|                  OFF   | 
---|                  ON    | 
---|                  R1    | 
---|                  R2    | 
---|                  R3    | 
---|                  L1    | 
---|                  L2    | 
---|                  L3    | 
+--|                  OFF   | 10000000
+--|                  ON    | 01000000
+--|                  R1    | 00100000
+--|                  R2    | 00010000
+--|                  R3    | 00001000
+--|                  L1    | 00000100
+--|                  L2    | 00000010
+--|                  L3    | 00000001
 --|                 --------------------
 --|
 --|
@@ -87,22 +87,67 @@ library ieee;
  
 entity thunderbird_fsm is 
   port(
-	
+     i_clk, i_reset  : in    std_logic;
+     i_left, i_right : in    std_logic;
+     o_lights_L      : out   std_logic_vector(2 downto 0);
+     o_lights_R      : out   std_logic_vector(2 downto 0)
   );
 end thunderbird_fsm;
 
 architecture thunderbird_fsm_arch of thunderbird_fsm is 
 
 -- CONSTANTS ------------------------------------------------------------------
-  
+ signal s_Q  : std_logic_vector(2 downto 0) := "000";
+ signal  s_Q_next  : std_logic_vector (2 downto 0) := "000";
+ 
 begin
 
 	-- CONCURRENT STATEMENTS --------------------------------------------------------	
+	s_Q_next(2) <= (not s_Q(2) and not s_Q(1) and not s_Q(0) and i_left and not i_right) or
+	               (not s_Q(2) and not s_Q(1) and not s_Q(0) and i_left and i_right) or
+	               (not s_Q(2) and s_Q(1) and not s_Q(0)) or
+	               (s_Q(2) and s_Q(1) and not s_Q(0));
+	s_Q_next(1) <= (not s_Q(2) and not s_Q(1) and not s_Q(0) and not i_left and i_right)or
+	               (not s_Q(2) and s_Q(1) and not s_Q(0)) or
+	               (s_Q(2) and not s_Q(1) and s_Q(0)) or
+	               (s_Q(2) and s_Q(1) and not s_Q(0));
+	s_Q_next(0) <= (not s_Q(2) and not s_Q(1) and not s_Q(0) and i_left and not i_right)or
+	               (not s_Q(2) and s_Q(1) and s_Q(0)) or
+	               (s_Q(2) and not s_Q(1)and s_Q(0)) or
+	               (S_Q(2) and s_Q(1) and not s_Q(0));
+
 	
+	o_Lights_L(2) <= (not s_Q(2) and not s_Q(1) and s_Q(0)) or
+	                 ( s_Q(2) and s_Q(1) and s_Q(0));
+	o_Lights_L(1) <= (not s_Q(2) and not s_Q(1) and s_Q(0)) or
+	                 (s_Q(2) and s_Q(1) and not s_Q(0)) or
+	                 (s_Q(2)and s_Q(1) and s_Q(0));
+	o_Lights_L(0) <= (not s_Q(2)and not s_Q(1) and s_Q(0)) or
+	                 (s_Q(2) and not s_Q(1) and s_Q(0)) or 
+	                 (s_Q(2)and s_Q(1)  and not s_Q(0)) or
+	                 (s_Q(2) and s_Q(1) and s_Q(0));
+	
+	o_Lights_R(2) <= (not s_Q(2) and not s_Q(1) and s_Q(0)) or
+	                 (not s_Q(2) and s_Q(1) and not s_Q(0)) or
+	                 (not s_Q(2) and s_Q(1) and s_Q(0)) or
+	                 (s_Q(2) and not s_Q(1) and not s_Q(0));
+	o_Lights_R(1) <= (not s_Q(2) and not s_Q(1) and s_Q(0)) or
+	                 (not s_Q(2) and s_Q(1) and s_Q(0)) or 
+	                 (s_Q(2) and not s_Q(1) and not s_Q(0));
+	o_Lights_R(0) <= (not s_Q(2) and not s_Q(1) and s_Q(0)) or
+	                 (s_Q(2) and not s_Q(1) and not s_Q(0));
     ---------------------------------------------------------------------------------
 	
 	-- PROCESSES --------------------------------------------------------------------
-    
+	register_proc : process ( i_clk, i_reset )
+    begin
+        if i_reset = '1' then
+          s_Q <= "10";    --Reset state is yellow
+         elsif (rising_edge(i_clk)) then
+          s_Q <= s_Q_next;
+        end if;
+
+    end process register_proc;    
 	-----------------------------------------------------					   
 				  
 end thunderbird_fsm_arch;
